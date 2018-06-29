38f1dac00a73532c080d89b345d12de05b72706d
Robert Johnson <rob5408@gmail.com>
2018-06-29 09:25:03 -0500

    Sid's Changes

--- SAiDoku/main.swift
+++ SAiDoku/main.swift
@@ -1,18 +1,35 @@
-//
-//  main.swift
-//  SAiDoku
-//
-//  Created by Robert Johnson on 6/27/18.
-//  Copyright © 2018 SAiD. All rights reserved.
-//
-
 import Foundation
 
-var array = [[[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","",""],],],[[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","",""],],],[[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","",""],],[["","",""],["","",""],["","", ""],],]]
+// var array = [[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],],[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],],[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0, 0],],]]
+
+struct members {
+    var Int row;
+    var Int column;
+    var Int region;
+}
+
+struct State {
+    
+    var data: Array<Int> = Array(repeating: 0, count: 81)
+    
+    var rows: Int[][];
+    var columns: Int[][];
+    var regions: Int[][];
+    
+    var cells: Array<members>;
+    
+    func coordOf(index: Int) -> (Int, Int, Int) {
+        
+    }
+
+    func indexOf(region: Int, row: Int, column: Int) -> Int {
+        return region * 9 + row * 3 + column
+    }
+
+}
 
 do {
     let data = try String(contentsOfFile: CommandLine.arguments[1])
-    // print(data)
     
     // NOTE: Normally you should run, not walk, away from a codebase with four nested for loops, but in this case I think it's ok
     for w in 0...2 {
@@ -21,15 +38,12 @@ do {
                 for z in 0...2 {
                     let value = (w * 27) + (x * 9) + (y * 3) + z
                     let char = String(data[data.index(data.startIndex, offsetBy: value)])
-                    // print(char)
                     // NOTE: x and y are transposed because I went board by board and qqwing went row by row
-                    array[w][y][x][z] = (char == ".") ? " " : char
+                    array[w][y][x][z] = Int(char) ?? 0
                 }
             }
         }
     }
-    
-    // print(array)
 } catch {
 }
 
@@ -45,20 +59,27 @@ print("""
 
 """)
 
-// We can use our information about a box, a row, a column
-
-
-
-func missingNumbers(existingNumbers: Array<Int>) -> Array<Int> {
-    return Array(Set([1,2,3,4,5,6,7,8,9]).subtracting(Set(existingNumbers)))
+func missingNumbers(_ existingNumbers: Array<Int>) -> Array<Int> {
+    return Array(Set(1...9).subtracting(Set(existingNumbers)))
 }
 
 func existingNumbersForRowsIn(board: Int, region: Int) -> Array<Int> {
+    var numbers: Array<Int> = []
     for x in 0...2 {
-        
+        for y in 0...2 {
+            if (array[board][x][region][y] != 0) {
+                numbers.append( array[board][x][region][y] );
+            }
+        }
     }
+    
+    return numbers
 }
-// array[0]
+
+print("Missing numbers from row 0: \( missingNumbers(existingNumbersForRowsIn(board: 0, region: 0))  )")
+
+
+
 
 func displayBoard() {
     
