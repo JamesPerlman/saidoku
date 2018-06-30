de7b529bc555b82df518ba27b87ac181f71b1667
JamesPerlman <jam.e.perl@gmail.com>
2018-06-29 21:33:20 -0500

    refactor main.swift

--- SAiDoku/main.swift
+++ SAiDoku/main.swift
@@ -1,52 +1,5 @@
 import Foundation
 
-// var array = [[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],],[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],],[[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0,0],],[[0,0,0],[0,0,0],[0,0, 0],],]]
-
-struct members {
-    var Int row;
-    var Int column;
-    var Int region;
-}
-
-struct State {
-    
-    var data: Array<Int> = Array(repeating: 0, count: 81)
-    
-    var rows: Int[][];
-    var columns: Int[][];
-    var regions: Int[][];
-    
-    var cells: Array<members>;
-    
-    func coordOf(index: Int) -> (Int, Int, Int) {
-        
-    }
-
-    func indexOf(region: Int, row: Int, column: Int) -> Int {
-        return region * 9 + row * 3 + column
-    }
-
-}
-
-do {
-    let data = try String(contentsOfFile: CommandLine.arguments[1])
-    
-    // NOTE: Normally you should run, not walk, away from a codebase with four nested for loops, but in this case I think it's ok
-    for w in 0...2 {
-        for x in 0...2 {
-            for y in 0...2 {
-                for z in 0...2 {
-                    let value = (w * 27) + (x * 9) + (y * 3) + z
-                    let char = String(data[data.index(data.startIndex, offsetBy: value)])
-                    // NOTE: x and y are transposed because I went board by board and qqwing went row by row
-                    array[w][y][x][z] = Int(char) ?? 0
-                }
-            }
-        }
-    }
-} catch {
-}
-
 // http://patorjk.com/software/taag/#p=testall&f=ANSI%20Shadow&t=SAiDoku
 print("""
 
@@ -58,63 +11,16 @@ print("""
 ╚══════╝╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝
 
 """)
+/*
+let filePath = CommandLine.arguments[1]
+guard let gameString = try? String(contentsOfFile: filePath) else
+{
+    print("BAD FILE")
+    exit(1)
+}*/
+let gameString = "984531.7261...7...257..98..3...6..1....37.92...9..5....3...6....45.18.961967..28."
 
-func missingNumbers(_ existingNumbers: Array<Int>) -> Array<Int> {
-    return Array(Set(1...9).subtracting(Set(existingNumbers)))
-}
-
-func existingNumbersForRowsIn(board: Int, region: Int) -> Array<Int> {
-    var numbers: Array<Int> = []
-    for x in 0...2 {
-        for y in 0...2 {
-            if (array[board][x][region][y] != 0) {
-                numbers.append( array[board][x][region][y] );
-            }
-        }
-    }
-    
-    return numbers
-}
-
-print("Missing numbers from row 0: \( missingNumbers(existingNumbersForRowsIn(board: 0, region: 0))  )")
-
-
-
-
-func displayBoard() {
-    
-    // Board Row, Board Column, Region Row, Region Column
-    
-    print("""
-        
-        ╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗
-        ║ \(array[0][0][0][0]) │ \(array[0][0][0][1]) │ \(array[0][0][0][2]) ║ \(array[0][1][0][0]) │ \(array[0][1][0][1]) │ \(array[0][1][0][2]) ║ \(array[0][2][0][0]) │ \(array[0][2][0][1]) │ \(array[0][2][0][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[0][0][1][0]) │ \(array[0][0][1][1]) │ \(array[0][0][1][2]) ║ \(array[0][1][1][0]) │ \(array[0][1][1][1]) │ \(array[0][1][1][2]) ║ \(array[0][2][1][0]) │ \(array[0][2][1][1]) │ \(array[0][2][1][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[0][0][2][0]) │ \(array[0][0][2][1]) │ \(array[0][0][2][2]) ║ \(array[0][1][2][0]) │ \(array[0][1][2][1]) │ \(array[0][1][2][2]) ║ \(array[0][2][2][0]) │ \(array[0][2][2][1]) │ \(array[0][2][2][2]) ║
-        ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
-        ║ \(array[1][0][0][0]) │ \(array[1][0][0][1]) │ \(array[1][0][0][2]) ║ \(array[1][1][0][0]) │ \(array[1][1][0][1]) │ \(array[1][1][0][2]) ║ \(array[1][2][0][0]) │ \(array[1][2][0][1]) │ \(array[1][2][0][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[1][0][1][0]) │ \(array[1][0][1][1]) │ \(array[1][0][1][2]) ║ \(array[1][1][1][0]) │ \(array[1][1][1][1]) │ \(array[1][1][1][2]) ║ \(array[1][2][1][0]) │ \(array[1][2][1][1]) │ \(array[1][2][1][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[1][0][2][0]) │ \(array[1][0][2][1]) │ \(array[1][0][2][2]) ║ \(array[1][1][2][0]) │ \(array[1][1][2][1]) │ \(array[1][1][2][2]) ║ \(array[1][2][2][0]) │ \(array[1][2][2][1]) │ \(array[1][2][2][2]) ║
-        ╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣
-        ║ \(array[2][0][0][0]) │ \(array[2][0][0][1]) │ \(array[2][0][0][2]) ║ \(array[2][1][0][0]) │ \(array[2][1][0][1]) │ \(array[2][1][0][2]) ║ \(array[2][2][0][0]) │ \(array[2][2][0][1]) │ \(array[2][2][0][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[2][0][1][0]) │ \(array[2][0][1][1]) │ \(array[2][0][1][2]) ║ \(array[2][1][1][0]) │ \(array[2][1][1][1]) │ \(array[2][1][1][2]) ║ \(array[2][2][1][0]) │ \(array[2][2][1][1]) │ \(array[2][2][1][2]) ║
-        ╟───┼───┼───╫───┼───┼───╫───┼───┼───╢
-        ║ \(array[2][0][2][0]) │ \(array[2][0][2][1]) │ \(array[2][0][2][2]) ║ \(array[2][1][2][0]) │ \(array[2][1][2][1]) │ \(array[2][1][2][2]) ║ \(array[2][2][2][0]) │ \(array[2][2][2][1]) │ \(array[2][2][2][2]) ║
-        ╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝
-        
-        """)
-}
-
-displayBoard()
-
-
-
-
-
-
-
+let game = SudokuGame(gameString)
+game.solve({ result in
+    print(result.description);
+})
